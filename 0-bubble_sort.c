5f1618037601aa66782439a109e63a997ce27836
walter-cumbucane <cumbucane.walter@gmail.com>
2023-10-23 19:10:11 +0200

    Let's get started

---
+++ 0-bubble_sort.c
@@ -0,0 +1,16 @@
+#include "sort.h"
+
+
+/**
+ * bubble_sort - sorts an array in ascending order
+ * @array: a pointer to the beginning of the array
+ * @size: size of the array
+ *
+ * Return: nothing
+ */
+
+
+void bubble_sort(int *array, size_t size)
+{
+	return;
+}
