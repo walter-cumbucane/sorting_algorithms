30d3ab052c637678980762f83dbfd5f3e900edbf
walter-cumbucane <cumbucane.walter@gmail.com>
2023-10-23 19:54:51 +0200

    Cooking Task 0

--- 0-bubble_sort.c
+++ 0-bubble_sort.c
@@ -28,8 +28,8 @@ void bubble_sort(int *array, size_t size)
 				swap = array[j];
 				array[j] = array[j + 1];
 				array[j + 1] = swap;
+				print_array(array, size);
 			}
-			print_array(array, size);
 		}
 	}
 }
